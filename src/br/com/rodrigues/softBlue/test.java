e1967c3c44c17744616797441065e116a9fb0964
rodrigues159 <57396876+rodrigues159@users.noreply.github.com>
2020-09-17 20:38:02 -0300

    Projeto criado

---
+++ src/br/com/rodrigues/softBlue/test.java
@@ -0,0 +1,17 @@
+package br.com.rodrigues.softBlue;
+
+public class test {
+
+	public static void main(String[] args) {
+		var v = "teste";
+		
+		String s = new String();
+		
+		s = v;
+		
+		System.out.println(v .toUpperCase());
+	
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Rodrigues
+ *
+ */
+module project {
+}
